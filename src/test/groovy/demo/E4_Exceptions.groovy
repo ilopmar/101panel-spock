ea464774b80c09a5e1d0f0da2c648ee7c31e40bc
Iván López <lopez.ivan@gmail.com>
2017-01-18 15:02:53 +0100

    Example 4: Exceptions

---
+++ src/test/groovy/demo/E4_Exceptions.groovy
@@ -0,0 +1,14 @@
+package demo
+
+import spock.lang.Specification
+
+class E4_Exceptions extends Specification {
+
+    void 'should throw an exception'() {
+        when:
+            Long.valueOf("foo")
+
+        then:
+            thrown NumberFormatException
+    }
+}
